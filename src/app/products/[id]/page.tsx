a1aeacc741121f7fba9001e57cb1e6d2ec7395e9
MinhLuu <minhluu15012000@gmail.com>
2024-12-11 07:27:33 +0000

    add cicd

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,11 +1,11 @@
 import productApiRequest from '@/apiRequests/product'
 import Image from 'next/image'
 import { Metadata, ResolvingMetadata } from 'next'
-import { cache } from 'react'
+
 import envConfig from '@/config'
 import { baseOpenGraph } from '@/app/shared-metadata'
 
-const getDetail = cache(productApiRequest.getDetail)
+
 
 type Props = {
   params: Promise<{ id: string }>
@@ -14,7 +14,7 @@ type Props = {
 
 export async function generateMetadata(props: Props, parent: ResolvingMetadata): Promise<Metadata> {
   const params = await props.params;
-  const { payload } = await getDetail(Number(params.id))
+  const { payload } = await productApiRequest.getDetail(Number(params.id))
   const product = payload.data
   const url = envConfig.NEXT_PUBLIC_URL + '/products/' + product.id
   return {
@@ -41,7 +41,7 @@ export default async function ProductDetail(props: Props) {
   const params = await props.params;
   let product = null
   try {
-    const { payload } = await getDetail(Number(params.id))
+    const { payload } = await productApiRequest.getDetail(Number(params.id))
     product = payload.data
   } catch (error) {}
 
